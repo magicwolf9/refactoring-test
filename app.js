f46b89ab0c00f1dce4c018fe10f83f65a298839d
Evgenii <e.kukhta@qiwi.com>
2018-12-02 14:19:43 +0300

    Some new paths

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
-express = require("express");
-axios = require("axios");
+const express = require("express");
+const axios = require("axios");
+const fs = require('fs');
 
 let app = express();
 
@@ -13,8 +14,40 @@ app.get('/path', async function (req, res) {
 
     const r = await axios.get(`http://google.com/search?q=${req.query.x}`);
 
-    console.log(r.status);
-    res.send(r.status);
+    res.send(r.data);
 });
+
+app.get('/path2', function (req, res) {
+    console.log(req.query.x, req.query.y, req.query.op);
+
+    let k = "";
+    if (req.query.op === "/") {
+        k = req.query.x / req.query.y;
+    } else if (req.query.op === "*") {
+        k = req.query.x * req.query.y;
+    } else if (req.query.op === "+") {
+        k = req.query.x + req.query.y;
+    } else if (req.query.op === "-") {
+        k = req.query.x - req.query.y;
+    } else {
+        k = "-";
+    }
+
+    res.send("" + k);
+});
+
+app.get('/path3', function (req, res) {
+    let i = fs.readFileSync('./file2.txt', 'utf8');
+    fs.writeFileSync('./file2.txt', ++i);
+
+
+    fs.readFile('./file.txt', 'utf8', function(err,data){
+        if (err) {
+            res.send('bad');
+        }
+        res.send(data);
+    });
+});
+
 
 app.listen(3000);
